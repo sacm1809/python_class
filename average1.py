6cdbfb7c63f77b456589c57f11bd32304d04797f
sofia chamorro m <sofia.chamorro@gmail.com>
2018-09-12 00:04:42 -0500

    suma y promedio de edades

---
+++ average1.py
@@ -0,0 +1,16 @@
+m = 1
+edades = 0
+while m <= 10:
+    edad=int(input("ingrese su edad "))
+    edades = edades + edad
+    m = m + 1
+promedio = edades /10
+print ("el promedio de las diez edades ingresadas es: ", promedio)
+
+i=0
+suma=0
+while i<=100:
+    suma = suma + i
+    i = i + 2
+print ("la suma de los numeros es igual a: ", suma)
+
